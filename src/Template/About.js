b71fedc0a4f46f05c099c12477a90e567f85b96c
nadws <nandw567@gmail.com>
2023-11-04 11:20:34 +0800

    dsadas

--- src/Template/About.js
+++ src/Template/About.js
@@ -29,19 +29,32 @@ function About() {
           <div className="bg-[#0D1018] border text-[#4a545f]  border-[#30363D] rounded-lg p-4 w-11/12 ">
             <div className="bg-[#0D1018] rounded-lg p-4 lg:w-12/12 ">
               <p className="font-Montserrat text-lg mb-4 cursor-pointer text-[#746B6D] hover:text-white hover:bg-[#6f7975]">
-                1 /**
+                1. /** Hello, I am a programmer based in Indonesia, and I have
+                been working in the field of programming since 2020.
               </p>
               <p className="font-Montserrat text-lg mb-4 cursor-pointer text-[#746B6D] hover:text-white hover:bg-[#6f7975]">
-                2 * Hello my name is nanda wahyudi
+                2 * I have a passion for technology and enjoy solving complex
+                problems through coding. Over the years, I have honed my skills
+                in these technologies and gained practical experience.
               </p>
               <p className="font-Montserrat text-lg mb-4 cursor-pointer text-[#746B6D] hover:text-white hover:bg-[#6f7975]">
-                3 * Full stack developer since 2020
+                3 * I specialize in using PHP with the Laravel framework, and I
+                am proficient in web development using React.js and Vue.js.
+                Additionally, I am highly skilled in working with SQL databases.
               </p>
               <p className="font-Montserrat text-lg mb-4 cursor-pointer text-[#746B6D] hover:text-white hover:bg-[#6f7975]">
-                4 * SQL/NoSQL database architecture, designing and developing
+                4 * I am always eager to learn and keep up with the latest
+                developments in the world of software development. In my free
+                time, I like to work on personal coding projects and explore new
+                programming concepts.
               </p>
               <p className="font-Montserrat text-lg mb-4 cursor-pointer text-[#746B6D] hover:text-white hover:bg-[#6f7975]">
-                5 * Very good at programming with PHP
+                5 * I am a dedicated and detail-oriented individual who is
+                committed to delivering high-quality software solutions.
+              </p>
+              <p className="font-Montserrat text-lg mb-4 cursor-pointer text-[#746B6D] hover:text-white hover:bg-[#6f7975]">
+                6 * I look forward to new challenges and opportunities in the
+                field of programming.
               </p>
             </div>
           </div>
